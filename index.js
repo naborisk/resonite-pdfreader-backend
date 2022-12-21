74ec84b0e7f10a105d208331e91adbead04f8f8e
Naborisk <naborisk@gmail.com>
2022-12-21 23:46:38 +0900

    change to send file information

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 const multer = require('multer')
 const path = require('path')
 const {spawn} = require('node:child_process')
@@ -9,20 +10,41 @@ const upload = multer({dest: 'uploads/'})
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('nya')
+  res.send('up!')
 })
 
 app.post('/upload', upload.single('pdf'), (req, res, next) => {
-  const pdftoppm = spawn('pdftoppm', [req.file.path, `out/out`, '-png'])
+  const filename = req.file.filename
+
+  // create working directory
+  fs.mkdirSync(filename)
+
+  // spawn pdftoppm process to convert pdf to png
+  const pdftoppm = spawn('pdftoppm', [req.file.path, `${filename}/out`, '-png'])
 
   pdftoppm.on('close', (code) => {
 
     console.log('pdftoppm process closed')
-    res.sendFile('./out/out-01.png', {root: path.join(__dirname)})
 
-    console.log('files sent to client, cleaning up...')
-    //spawn('rm', ['-f', req.file.path, "./out/*"], {shell: true})
+    const files = fs.readdirSync(filename)
+
+    // return file information used for download
+    res.json({
+      pageCount: files.length,
+      path: filename,
+      files
+    })
+
+    console.log('files infortmation sent to client, cleaning up...')
+    // remove uploaded file
+    spawn('rm', [req.file.path])
+
+    // remove converted files
+    spawn('rm', ['-r', filename])
   })
+})
+
+app.get('/download', (res, req) => {
 })
 
 app.listen(port, () => {
